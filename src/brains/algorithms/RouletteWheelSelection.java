896f4f24e5aa8560efac098b22fda7e41ab6a1ea
Robrecht Cannoodt <brechtus@gmail.com>
2012-11-20 23:40:10 +0100

    * Added a roulette wheel selection algorithm * Modified the FastSlamOccupancyGrid algorithm to make use of it.

---
+++ src/brains/algorithms/RouletteWheelSelection.java
@@ -0,0 +1,51 @@
+package brains.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import brains.Particle;
+
+public class RouletteWheelSelection {
+	private Random random;
+	
+	public RouletteWheelSelection() {
+		random = new Random();
+	}
+	
+	private Particle nextRandomParticle(List<Particle> particles, double sumWeight) {
+		double x = random.nextDouble();
+		int i = 0;
+		
+		double normProb = particles.get(i).getWeight() / sumWeight;
+		while (x > normProb) {
+			x -= normProb;
+			i++;
+			normProb = particles.get(i).getWeight() / sumWeight;
+		}
+		return particles.get(i);
+	}
+	
+	private double calculateSumWeight(List<Particle> particles) {
+		double sumWeight = 0;
+		for (Particle p : particles) {
+			sumWeight += p.getWeight();
+		}
+		return sumWeight;
+	}
+	
+	public Particle nextRandomParticle(List<Particle> particles) {
+		double sumWeight = calculateSumWeight(particles);
+		
+		return nextRandomParticle(particles, sumWeight);
+	}
+	
+	public ArrayList<Particle> nextRandomParticles(List<Particle> particles, int n) {
+		double sumWeight = calculateSumWeight(particles);
+		
+		ArrayList<Particle> newParticles = new ArrayList<Particle>();
+		for (int i = 0; i < n; i++) 
+			newParticles.add(nextRandomParticle(particles, sumWeight));
+		return newParticles;
+	}
+}
---
+++ test/test/brains/algorithms/RouletteWheelSelectionTest.java
@@ -0,0 +1,65 @@
+package test.brains.algorithms;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import brains.Particle;
+import brains.algorithms.RouletteWheelSelection;
+
+public class RouletteWheelSelectionTest {
+	private List<Particle> particles;
+	private RouletteWheelSelection rws;
+
+	@Before
+	public void setUp() throws Exception {
+		particles = new ArrayList<Particle>();
+		particles.add(new Particle(null, null, 0.1));
+		particles.add(new Particle(null, null, 0.5));
+		particles.add(new Particle(null, null, 0.9));
+		particles.add(new Particle(null, null, 1.5));
+		particles.add(new Particle(null, null, 1.9));
+		particles.add(new Particle(null, null, 2.5));
+		
+		rws = new RouletteWheelSelection();
+	}
+
+	@Test
+	public void testNextRandomParticles() {
+		int amount = 1000000;
+		List<Particle> sample = rws.nextRandomParticles(particles, amount);
+	
+		assertEquals("Did not receive the right amount of samples", amount, sample.size());
+		
+		double sumWeights = 0;
+		Map<Double, Integer> counting = new HashMap<Double, Integer>();
+		for (Particle p : particles) {
+			counting.put(p.getWeight(), 0);
+			sumWeights += p.getWeight();
+		}
+		for (Particle p : sample) {
+			double key = p.getWeight();
+			counting.put(key, counting.get(key)+1);
+		}
+		for (Particle p : particles) {
+			double key = p.getWeight();
+			double probability = key / sumWeights;
+			double occurrenceRate = ((double) counting.get(key)) / amount;
+			double diff = probability - occurrenceRate; 
+			
+			String msg = 
+				"Particle with key " + key + " occurred too often. (With an occurrence rate of " +
+				occurrenceRate + " instead of the expected rate of " + probability + ".";
+			assertTrue(msg, diff < 0.01);
+			System.out.println("Particle " + key + ", probability - occurrence_rate = " + diff);
+		}
+	}
+
+}

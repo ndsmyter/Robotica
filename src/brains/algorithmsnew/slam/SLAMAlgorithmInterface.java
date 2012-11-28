2fc60dc31ea3d188bb28641aa435ee9193a413fb
Robrecht Cannoodt <brechtus@gmail.com>
2012-11-28 17:24:39 +0100

    Removed unneeded comment

--- src/brains/algorithmsnew/slam/SLAMAlgorithmInterface.java
+++ src/brains/algorithmsnew/slam/SLAMAlgorithmInterface.java
@@ -5,7 +5,6 @@ import java.util.List;
 import brains.Particle;
 
 public interface SLAMAlgorithmInterface {
-	// eventueel een iets strictere interface
 	public List<Particle> execute(List<Particle> particles, int[] u, int[] z);
 	public void reset();
 }

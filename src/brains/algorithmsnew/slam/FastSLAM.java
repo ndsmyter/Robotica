d67b5b8b936ba03671f3e21294a402f9fbb2505f
Robrecht Cannoodt <brechtus@gmail.com>
2012-12-05 14:04:30 +0100

    Implemented the measurementModelMap

--- src/brains/algorithmsnew/slam/FastSLAM.java
+++ src/brains/algorithmsnew/slam/FastSLAM.java
@@ -17,14 +17,15 @@ import common.Utils;
 
 public class FastSLAM implements SLAMAlgorithmInterface {
 	private RouletteWheelSelection roulette;
-	
+
 	public FastSLAM() {
 		roulette = new RouletteWheelSelection();
 	}
-	
+
 	@Override
-	public void reset() {}
-	
+	public void reset() {
+	}
+
 	/*
 	 * For quick reference: pg 478
 	 */
@@ -34,12 +35,12 @@ public class FastSLAM implements SLAMAlgorithmInterface {
 
 		for (Particle p : particles) {
 			MapStructure map = p.getMap();
-			
+
 			RobotState newPosition = sampleMotionModel(u, map.getPosition());
 			map.setPosition(newPosition);
 			double weight = measurementModelMap(z, map);
 			MapStructure newMap = updatedOccupancyGrid(z, map);
-			
+
 			updatedParticles.add(new Particle(newMap, weight));
 		}
 
@@ -51,42 +52,45 @@ public class FastSLAM implements SLAMAlgorithmInterface {
 
 	/**
 	 * TODO Stef
-	 * @param u de bewegingsvector u_t. u[0] → voorwaarts bewegen, u[1] → draaien
-	 * @param x de state van de robot
+	 * 
+	 * @param u
+	 *            de bewegingsvector u_t. u[0] → voorwaarts bewegen, u[1] →
+	 *            draaien
+	 * @param x
+	 *            de state van de robot
 	 * @return
 	 */
 	public RobotState sampleMotionModel(int[] u, RobotState x) {
-		// Note van Robrecht: Ik heb hier tijdelijk al iets ingestoken 
-		// zodanig dat onze simulatie al werkt. Het geen ik hier doe gaat 
-		// geen rekening houden met de variantie op onze metingen, wat 
+		// Note van Robrecht: Ik heb hier tijdelijk al iets ingestoken
+		// zodanig dat onze simulatie al werkt. Het geen ik hier doe gaat
+		// geen rekening houden met de variantie op onze metingen, wat
 		// wel de bedoelig is. Dit stukje code veronderstelt dus dat
 		// onze meting 100% juist is.
 		RobotState tmp = new RobotState(x.x, x.y, x.dir);
-		
-		
-		
+
 		if (u[0] != 0)
 			tmp = Utils.driveForward(tmp, u[0]);
-		
+
 		if (u[1] != 0)
 			tmp.dir = (tmp.dir + u[1] + 360) % 360;
-		
+
 		return tmp;
 	}
-	
+
 	public int sample(int b2) {
 		double result = 0;
-		int r = (int)Math.round(Math.sqrt(b2));
+		int r = (int) Math.round(Math.sqrt(b2));
 		Random rand = new Random();
-		for ( int i= 0 ; i < 12 ; i++){
-			result += (-r + rand.nextInt(2 * r) );
+		for (int i = 0; i < 12; i++) {
+			result += (-r + rand.nextInt(2 * r));
 		}
 		result /= 2;
-		return (int)Math.round(result);
+		return (int) Math.round(result);
 	}
 
 	/**
 	 * TODO Robrecht
+	 * 
 	 * @param z
 	 * @param x
 	 * @param m
@@ -103,51 +107,49 @@ public class FastSLAM implements SLAMAlgorithmInterface {
             Point measurement = Utils.pointToGrid(Utils.sensorDataToPoint(robotState, z[i], s));
             ArrayList<Point> path = Utils.getPath(sensorState, s.zMax);
             for (Point p : path) {
-            	double x = m.get(p);
-            	// need to find a better value for y
-            	double y;
-            	if (measurement.equals(p)) {
-            		y = 1.0;
-            	} else {
-            		y = 0.0;
-            	}
-            	sum += x * y;
-            }
-        }
-		return sum;
-	}
-	
-	public MapStructure updatedOccupancyGrid(int[] z, MapStructure m) {
-        MapStructure mapNew = m.clone();
-        RobotState robotState = m.getPosition();
-        for (int i = 0; i < RoombaConfig.SENSORS.length; i++) {
-            Sensor s = RoombaConfig.SENSORS[i];
-            RobotState sensorState = Utils.getSensorState(robotState, s);
-            Point measurement = Utils.pointToGrid(Utils.sensorDataToPoint(
-                    robotState, z[i], s));
-            ArrayList<Point> path = Utils.getPath(sensorState, s.zMax);
-            for (Point p : path) {
-                double logOdds = m.getLogOdds(p)
-                        + inverseSensorModel(m, p, measurement, sensorState,
-                        z[i], s);
-                mapNew.putLogOdds(p, logOdds);
-            }
-        }
-        return mapNew;
-    }
+            	double x = m.getLogOdds(p);//m.get(p);
+            	
+            	double y = inverseSensorModel(p, measurement, sensorState, z[i], s);
 
-    public static double inverseSensorModel(MapStructure m, Point p,
-            Point measurement, RobotState sensorState, int z, Sensor s) {
-        double result = 0;
-        int r = Utils.euclideanDistance(
-                new Point(sensorState.x, sensorState.y), p);
-        if (r > Math.min(s.zMax, z) + Config.GRID_CELL_SIZE) {
-            result = 0;
-        } else if (z < s.zMax && p.equals(measurement)) {
-            result = 0.6; // p(occupied | z) = 0.8 => log 0.8/0.2 = log 4 = 0.6
-        } else if (r < z) {
-            result = -0.6; // p(occupied | z) = 0.2 => 0.2/0.8 = log 0.25 = -0.6
+            	sum += x + y;
+            }
         }
-        return result;
-    }
+    	return sum;
+	}
+
+	public MapStructure updatedOccupancyGrid(int[] z, MapStructure m) {
+		MapStructure mapNew = m.clone();
+		RobotState robotState = m.getPosition();
+		for (int i = 0; i < RoombaConfig.SENSORS.length; i++) {
+			Sensor s = RoombaConfig.SENSORS[i];
+			RobotState sensorState = Utils.getSensorState(robotState, s);
+			Point measurement = Utils.pointToGrid(Utils.sensorDataToPoint(
+					robotState, z[i], s));
+			ArrayList<Point> path = Utils.getPath(sensorState, s.zMax);
+			for (Point p : path) {
+				double logOdds = m.getLogOdds(p)
+						+ inverseSensorModel(p, measurement, sensorState, z[i],
+								s);
+				mapNew.putLogOdds(p, logOdds);
+			}
+		}
+		return mapNew;
+	}
+
+	public static double inverseSensorModel(Point p, Point measurement,
+			RobotState sensorState, int z, Sensor s) {
+		double result;
+		int r = Utils.euclideanDistance(
+				new Point(sensorState.x, sensorState.y), p);
+		if (r > Math.min(s.zMax, z) + Config.GRID_CELL_SIZE) {
+			result = 0;
+		} else if (z < s.zMax && p.equals(measurement)) {
+			result = 0.6; // p(occupied | z) = 0.8 => log 0.8/0.2 = log 4 = 0.6
+		} else if (r < z) {
+			result = -0.6; // p(occupied | z) = 0.2 => 0.2/0.8 = log 0.25 = -0.6
+		} else {
+			result = 0;
+		}
+		return result;
+	}
 }

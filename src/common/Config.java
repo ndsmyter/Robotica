ad72ec0fe0c6b19a80ca1a31197756916bda1ae5
Robrecht Cannoodt <brechtus@gmail.com>
2012-12-05 16:32:01 +0100

    FastSLAM stef methode verkeerde manier van discretizeren aangepast

--- src/common/Config.java
+++ src/common/Config.java
@@ -8,9 +8,9 @@ public class Config {
 
 	public static final int GRID_CELL_SIZE = 10;
 	public static final int GOAL_REACHED_TRESHOLD = 10;
-	public static final int NUMBER_OF_PARTICLES = 50;
-	public static final double ALPHA1 = 0.5;
-	public static final double ALPHA2 = 0.5;
+	public static final int NUMBER_OF_PARTICLES = 500;
+	public static final double ALPHA1 = 0.2;
+	public static final double ALPHA2 = 0.2;
 	
 
 	public static final boolean SIMULATED_NOISE = false;

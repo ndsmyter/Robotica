ebb007bc4c2032b61721ac69b314105f05dc6a4c
Nicolas De Smyter <nicolasdesmyter@gmail.com>
2012-12-18 17:07:41 +0100

    Changed some of the variables

--- src/common/Config.java
+++ src/common/Config.java
@@ -6,27 +6,29 @@ public class Config {
 	 ******************/
 	public static final int GRID_CELL_SIZE = 100;
 	public static final int GOAL_REACHED_TRESHOLD = 100;
-	public static final int NUMBER_OF_PARTICLES = 1000;
-	
-    /********************
+	public static final int NUMBER_OF_PARTICLES = 5;
+
+	/********************
 	 * Simulator config *
 	 ********************/
 	public static final double SIMULATED_MOVEMENT_NOISE_PCT = 0;
 	public static final double SIMULATED_ROTATION_NOISE_PCT = 0;
 	public static final double SIMULATED_SENSOR_NOISE_PCT = 0;
-	
+
 	public static final int SIMULATED_STEP_SIZE = 10;
-	
+
 	/*******************
 	 * FastSLAM config *
 	 *******************/
 	public static final int ITERATIONS_PER_RESAMPLE = 5;
-	public static final double ALPHA1 = 0;
-	public static final double ALPHA2 = 0;
-	public static final double LOGODD_OCCUPIED_CORRECT = 1.28; // p = 0.95 //0.6; // p(occupied | z) = 0.8 => log 0.8/0.2 = log 4 = 0.6
-        public static final double LOGODD_OCCUPIED_WRONG = -1.28; // p(occupied | z) = 0.2 => 0.2/0.8 = log 0.25 = -0.6
-        public static final double LOGODD_START = 0;
-        
+	public static final double ALPHA1 = 0.01;
+	public static final double ALPHA2 = 0.01;
+	// p(occupied | z) = 0.9 => log 0.9/0.1 = 0.95
+	public static final double LOGODD_OCCUPIED_CORRECT = 0.95;
+	// p(occupied | z) = 0.1 => log 0.1/0.9 = -0.95
+	public static final double LOGODD_OCCUPIED_WRONG = -0.95;
+	public static final double LOGODD_START = 0;
+
 	/**************
 	 * Bug config *
 	 **************/
@@ -34,7 +36,7 @@ public class Config {
 	public static final int BUG_STEP = 100;
 	public static final int BUG_TURN = 15;
 	public static final int BUG_SPIRAL = 200;
-	
+
 	public static final boolean BUG_EXPLORE_OBSTACLES_MORE = true;
 	public static final int BUG_OBST_EXPLORE_TURN = 90;
 	public static final int BUG_OBST_EXPLORE_ITERATIONS = 5;

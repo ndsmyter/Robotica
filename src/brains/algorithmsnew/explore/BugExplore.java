a1b907f1442ec0dc0f5f02a6e06e91cc2a905f7f
Stef Trenson <steftrenson@gmail.com>
2012-12-19 10:29:52 +0100

    changed begin direction of spiral

--- src/brains/algorithmsnew/explore/BugExplore.java
+++ src/brains/algorithmsnew/explore/BugExplore.java
@@ -51,7 +51,7 @@ public class BugExplore extends ExploreAlgorithmInterface {
 		// goals.add(new Point(0, 0));
 
 		// initialise spiral
-		dir = 0;
+		dir = 1;
 		goal = new Point(0, 0);
 		goalIndex = 0;
 		exploreCounter = 0;
@@ -204,17 +204,17 @@ public class BugExplore extends ExploreAlgorithmInterface {
 			}
 		}
 
-		System.out.println("Next goal: " + goal.x + " : " + goal.y);
+		System.out.println("Next goal: " + goal.x + " : " + goal.y +  " " + dir);
 
 		return goal;
 	}
 
 	private Point getNextGoal() {
 		if (dir == 0) { // UP
-			goal.y = Math.abs(goal.x) + Config.BUG_SPIRAL;
+			goal.y = Math.abs(goal.x);
 			dir = 1;
 		} else if (dir == 1) { // RIGHT
-			goal.x = goal.y;
+			goal.x = Math.abs(goal.y) + Config.BUG_SPIRAL;
 			dir = 2;
 		} else if (dir == 2) { // DOWN
 			goal.y = -goal.x;
